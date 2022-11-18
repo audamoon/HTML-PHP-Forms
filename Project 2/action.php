978cb4693de6db60888f408c9c4734f231bfbd53
audamoon <audamoon@gmail.com>
2022-11-18 22:07:26 +0300

    create html/css basics

---
+++ Project 2/action.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Project 2</title>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <link href="style.css" rel="stylesheet">
+</head>
+<body>
+    <div id="all-content">
+        <form action="action.php" method="POST">
+            <div id="appeal-form">
+            <label class="standart" for="full_name">Фамилия, Имя, Отчество
+                <span class="asterisk">*</span>
+            </label>
+            <input type="text" id="full_name" name="full_name" placeholder="Введите ФИО...">
+            <label class="standart" for="email">E-mail
+                <span class="asterisk">*</span>
+            </label>
+            <input type="email" id="email" name="email" placeholder="Введите e-mail">
+            </div>
+            <div id="submit-button-div">
+                <input type="submit" id="button">
+            </div>
+        </form>
+</div>
+    </body>
+</html>
---
+++ Project 2/printhello.php
@@ -0,0 +1,3 @@
+<?php
+echo "Hello, {$_POST['full_name']}";
+?>
